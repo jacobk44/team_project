6f0e8eb506ea7e60714e47b7d9066c57f9e681c3
jacobk44 <jacobkwartengamoah@gmail.com>
2026-04-04 23:11:00 -0700

    done with this week work

--- my-app/app/services/page.tsx
+++ my-app/app/services/page.tsx
@@ -1,3 +1,16 @@
-export default function Page(){
-    return <p>Services Page</p>
+// app/page.tsx
+import Hero from "@/app/ui/services/hero";
+
+
+export default function Home() {
+  return (
+    <main>
+      <Hero
+        title="Services"
+        description="Home and Moving cleaning services, customizable to suit your needs."
+        buttonText="BOOK NOW"
+        image="/clean_home.jpg"
+      />
+    </main>
+  );
 }
