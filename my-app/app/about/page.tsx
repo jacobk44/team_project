9a55657c0b2c87e07e43555794ec4b89c2ef6de1
jacobk44 <jacobkwartengamoah@gmail.com>
2026-04-15 11:47:56 -0700

    fixed my import

--- my-app/app/about/page.tsx
+++ my-app/app/about/page.tsx
@@ -1,6 +1,6 @@
 // app/page.tsx
 import Hero from "@/app/ui/about/hero";
-import ContactCta from "@/app/ui/layout/contact-cta";
+import ContactCta from "@/app/ui/layouts/contact-cta";
 
 
 export default function Home() {
